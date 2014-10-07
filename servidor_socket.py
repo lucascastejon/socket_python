3f3096e386d4ec2e509dc2ba71142e77fd917ff2
Lucas Castejon <lucascastejon@gmail.com>
2014-10-07 11:18:32 -0300

    [server] Added the sent request

--- servidor_socket.py
+++ servidor_socket.py
@@ -1,9 +1,10 @@
 import socket
 
-HOST = 'localhost'     # Endereco IP do Servidor
-PORT = 3030            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = '192.168.0.28'     # Endereco IP do Servidor
+PORT = 8080            # Porta que o Servidor esta
+tcp  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
+
 tcp.bind(orig)
 tcp.listen(1)
 
@@ -12,9 +13,11 @@ while True:
     con, cliente = tcp.accept()
     print 'Concetado por', cliente
     while True:
-        msg = con.recv(1024)
+        msg       = con.recv(1024) # recebe a mensagem
+        msg_send  = con.send(msg)	 # envia a mensagem
         if not msg: break
         print cliente, msg
     print 'Finalizando conexao do cliente', cliente
     con.close()
-print "Finalizado"
+
+print "Servidor Finalizado"
